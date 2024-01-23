bab7aa3cbe58a5ef0162945b70b03c7772ad83a7
Mamdasn <mhsaber1375@gmail.com>
2024-01-23 16:31:14 +0100

    Initialize `sphinx` doc builder config files
    + conf.py: sphinx builder configurations
    + index.rst: master document
    + modules.rst: modules configs

---
+++ docs/source/conf.py
@@ -0,0 +1,59 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Add project roots to PATH -----------------------------------------------
+
+import os
+import sys
+
+# Get current working directory
+cwd = os.getcwd()
+# Get the root dir by going back two times
+project_root = os.path.dirname(os.path.dirname(cwd))
+
+# Get the delivery_tools root dir
+delivery_tools_root = os.path.join(project_root, "delivery_tools")
+
+# Get the tests root dir
+tests_root = os.path.join(project_root, "tests")
+
+# Add the root dir to the system path
+sys.path.insert(0, delivery_tools_root)
+sys.path.insert(0, tests_root)
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Delivery Fee Calculator Flask App'
+copyright = '2024, Mamdasan Sabrian'
+author = 'Mamdasan Sabrian'
+version = '1.0'
+release = '1.0.0-alpha'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ["sphinx.ext.autodoc", ]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+# The suffix of source filenames.
+source_suffix = ".rst"
+
+# The master toctree document.
+master_doc = "index"
+
+# The name of the Pygments (syntax highlighting) style to use.
+pygments_style = "sphinx"
+
+# Set sorting of methods based on the source (default is alphabetical)
+autodoc_member_order = "bysource"
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
---
+++ docs/source/modules.rst
@@ -0,0 +1,14 @@
+Delivery Tools Module
+=====================
+
+.. automodule:: delivery_tools
+   :members:
+
+Tests Module
+============
+
+.. automodule:: test_delivery_fee
+   :members:
+
+.. automodule:: test_delivery_fee_http_api
+   :members:
