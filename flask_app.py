8b97a9bfb30e62b35a160ca834ff79e63f59f1ce
Mamdasan Sabrian <mhsaber1375@gmail.com>
2024-01-23 05:16:05 +0100

    Check input query to have all the needed fields
    Fields: cart_value, delivery_distance, number_of_items, time
    + turn the flask debug off

--- flask_app.py
+++ flask_app.py
@@ -7,9 +7,18 @@ app = Flask(__name__)
 @app.route("/", methods=["POST"])
 def get_data():
     data = request.get_json(force=True)
+    if not delivery_data_integrity_check(data):
+        return (
+            jsonify(
+                {
+                    "Error": "Incorrect input. Your data query should have the following fields: cart_value, delivery_distance, number_of_items and time"
+                }
+            ),
+            400,
+        )
     delivery_fee = delivery_fee_calculator(data)
     return jsonify(delivery_fee)
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
