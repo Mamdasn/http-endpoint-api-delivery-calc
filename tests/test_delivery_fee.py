34a6dcec4a32e58fde20b7552aa87cb81d977091
Mamdasan Sabrian <mhsaber1375@gmail.com>
2024-01-23 05:27:09 +0100

    Use unittests that provides testing tools
    Like assertEqual, assertRaises, assertTrue, assertFalse

--- tests/test_delivery_fee.py
+++ tests/test_delivery_fee.py
@@ -1,169 +1,162 @@
 import sys
 import os
+import unittest
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from delivery_tools import delivery_fee_calculator
 
 
-def test_fill_10_euro_gap_in_cart_value():
-    query = {
-        "cart_value": 890,
-        "delivery_distance": 1,
-        "number_of_items": 1,
-        "time": "2024-01-15T13:00:00Z",
-    }
-    expected_delivery_fee = 310
-    delivery_fee = delivery_fee_calculator(query)
-    delivery_fee_value = delivery_fee.get("delivery_fee")
-    assert delivery_fee_value == expected_delivery_fee
+class DeliveryFeeCalculatorTests(unittest.TestCase):
+    def test_fill_10_euro_gap_in_cart_value(self):
+        """Test the surcharge calculation for cart values less than 10€."""
+        queries = (
+            {
+                "cart_value": 890,
+                "delivery_distance": 1,
+                "number_of_items": 1,
+                "time": "2024-01-15T13:00:00Z",
+            },
+        )
+        expected_delivery_fees = (310,)
+        self.check_delivery_fee(queries, expected_delivery_fees)
 
+    def test_excess_500m_fee(self):
+        """Test additional delivery fees for every 500 meters beyond the first 1000 meters."""
+        queries = (
+            {
+                "cart_value": 1000,
+                "delivery_distance": 1000,
+                "number_of_items": 1,
+                "time": "2024-01-15T13:00:00Z",
+            },
+            {
+                "cart_value": 1000,
+                "delivery_distance": 1499,
+                "number_of_items": 1,
+                "time": "2024-01-15T13:00:00Z",
+            },
+            {
+                "cart_value": 1000,
+                "delivery_distance": 1500,
+                "number_of_items": 1,
+                "time": "2024-01-15T13:00:00Z",
+            },
+            {
+                "cart_value": 1000,
+                "delivery_distance": 1501,
+                "number_of_items": 1,
+                "time": "2024-01-15T13:00:00Z",
+            },
+        )
+        expected_delivery_fees = (200, 300, 300, 400)
 
-def test_excess_500m_fee():
-    queries = (
-        {
-            "cart_value": 1000,
-            "delivery_distance": 1000,
-            "number_of_items": 1,
-            "time": "2024-01-15T13:00:00Z",
-        },
-        {
-            "cart_value": 1000,
-            "delivery_distance": 1499,
-            "number_of_items": 1,
-            "time": "2024-01-15T13:00:00Z",
-        },
-        {
-            "cart_value": 1000,
-            "delivery_distance": 1500,
-            "number_of_items": 1,
-            "time": "2024-01-15T13:00:00Z",
-        },
-        {
-            "cart_value": 1000,
-            "delivery_distance": 1501,
-            "number_of_items": 1,
-            "time": "2024-01-15T13:00:00Z",
-        },
-    )
-    expected_delivery_fees = (200, 300, 300, 400)
+        self.check_delivery_fee(queries, expected_delivery_fees)
 
-    for expected_delivery_fee, query in zip(expected_delivery_fees, queries):
-        delivery_fee = delivery_fee_calculator(query)
-        delivery_fee_value = delivery_fee.get("delivery_fee")
-        assert delivery_fee_value == expected_delivery_fee
+    def test_excess_bulk_fee(self):
+        """Test the surcharge for bulk orders based on the number of items in the cart."""
+        queries = (
+            {
+                "cart_value": 1000,
+                "delivery_distance": 1000,
+                "number_of_items": 4,
+                "time": "2024-01-15T13:00:00Z",
+            },
+            {
+                "cart_value": 1000,
+                "delivery_distance": 1000,
+                "number_of_items": 5,
+                "time": "2024-01-15T13:00:00Z",
+            },
+            {
+                "cart_value": 1000,
+                "delivery_distance": 1000,
+                "number_of_items": 10,
+                "time": "2024-01-15T13:00:00Z",
+            },
+            {
+                "cart_value": 1000,
+                "delivery_distance": 1000,
+                "number_of_items": 13,
+                "time": "2024-01-15T13:00:00Z",
+            },
+            {
+                "cart_value": 1000,
+                "delivery_distance": 1000,
+                "number_of_items": 14,
+                "time": "2024-01-15T13:00:00Z",
+            },
+        )
+        expected_delivery_fees = (200, 250, 500, 770, 820)
 
+        self.check_delivery_fee(queries, expected_delivery_fees)
 
-def test_excess_bulk_fee():
-    queries = (
-        {
-            "cart_value": 1000,
-            "delivery_distance": 1000,
-            "number_of_items": 4,
-            "time": "2024-01-15T13:00:00Z",
-        },
-        {
-            "cart_value": 1000,
-            "delivery_distance": 1000,
-            "number_of_items": 5,
-            "time": "2024-01-15T13:00:00Z",
-        },
-        {
-            "cart_value": 1000,
-            "delivery_distance": 1000,
-            "number_of_items": 10,
-            "time": "2024-01-15T13:00:00Z",
-        },
-        {
-            "cart_value": 1000,
-            "delivery_distance": 1000,
-            "number_of_items": 13,
-            "time": "2024-01-15T13:00:00Z",
-        },
-        {
-            "cart_value": 1000,
-            "delivery_distance": 1000,
-            "number_of_items": 14,
-            "time": "2024-01-15T13:00:00Z",
-        },
-    )
-    expected_delivery_fees = (200, 250, 500, 770, 820)
+    def test_15_euro_cap(self):
+        """Test the maximum delivery fee cap of 15€, including surcharges."""
+        queries = (
+            {
+                "cart_value": 1000,
+                "delivery_distance": 7001,
+                "number_of_items": 4,
+                "time": "2024-01-15T13:00:00Z",
+            },
+            {
+                "cart_value": 1000,
+                "delivery_distance": 7001,
+                "number_of_items": 12,
+                "time": "2024-01-15T13:00:00Z",
+            },
+        )
+        expected_delivery_fees = (1500, 1500)
 
-    for expected_delivery_fee, query in zip(expected_delivery_fees, queries):
-        delivery_fee = delivery_fee_calculator(query)
-        delivery_fee_value = delivery_fee.get("delivery_fee")
-        assert delivery_fee_value == expected_delivery_fee
+        self.check_delivery_fee(queries, expected_delivery_fees)
 
+    def test_200_euro_free_delivery(self):
+        """Test free delivery for cart values equal to or exceeding 200€."""
+        queries = (
+            {
+                "cart_value": 19999,
+                "delivery_distance": 1000,
+                "number_of_items": 4,
+                "time": "2024-01-15T13:00:00Z",
+            },
+            {
+                "cart_value": 20000,
+                "delivery_distance": 7001,
+                "number_of_items": 4,
+                "time": "2024-01-15T13:00:00Z",
+            },
+        )
+        expected_delivery_fees = (200, 0)
 
-def test_15_euro_cap():
-    queries = (
-        {
-            "cart_value": 1000,
-            "delivery_distance": 7001,
-            "number_of_items": 4,
-            "time": "2024-01-15T13:00:00Z",
-        },
-        {
-            "cart_value": 1000,
-            "delivery_distance": 7001,
-            "number_of_items": 12,
-            "time": "2024-01-15T13:00:00Z",
-        },
-    )
-    expected_delivery_fees = (1500, 1500)
+        self.check_delivery_fee(queries, expected_delivery_fees)
 
-    for expected_delivery_fee, query in zip(expected_delivery_fees, queries):
-        delivery_fee = delivery_fee_calculator(query)
-        delivery_fee_value = delivery_fee.get("delivery_fee")
-        assert delivery_fee_value == expected_delivery_fee
+    def test_friday_rush(self):
+        """Test increased delivery fees during the Friday rush hours (3 - 7 PM UTC)."""
+        queries = (
+            {
+                "cart_value": 1000,
+                "delivery_distance": 1000,
+                "number_of_items": 4,
+                "time": "2024-01-19T14:59:59Z",
+            },
+            {
+                "cart_value": 1000,
+                "delivery_distance": 1000,
+                "number_of_items": 4,
+                "time": "2024-01-19T15:00:00Z",
+            },
+            {
+                "cart_value": 1000,
+                "delivery_distance": 1000,
+                "number_of_items": 4,
+                "time": "2024-01-19T19:00:00Z",
+            },
+        )
+        expected_delivery_fees = (200, 240, 240)
+        self.check_delivery_fee(queries, expected_delivery_fees)
 
-
-def test_200_euro_free_delivery():
-    queries = (
-        {
-            "cart_value": 19999,
-            "delivery_distance": 1000,
-            "number_of_items": 4,
-            "time": "2024-01-15T13:00:00Z",
-        },
-        {
-            "cart_value": 20000,
-            "delivery_distance": 7001,
-            "number_of_items": 4,
-            "time": "2024-01-15T13:00:00Z",
-        },
-    )
-    expected_delivery_fees = (200, 0)
-
-    for expected_delivery_fee, query in zip(expected_delivery_fees, queries):
-        delivery_fee = delivery_fee_calculator(query)
-        delivery_fee_value = delivery_fee.get("delivery_fee")
-        assert delivery_fee_value == expected_delivery_fee
-
-
-def test_friday_rush():
-    queries = (
-        {
-            "cart_value": 1000,
-            "delivery_distance": 1000,
-            "number_of_items": 4,
-            "time": "2024-01-19T14:59:59Z",
-        },
-        {
-            "cart_value": 1000,
-            "delivery_distance": 1000,
-            "number_of_items": 4,
-            "time": "2024-01-19T15:00:00Z",
-        },
-        {
-            "cart_value": 1000,
-            "delivery_distance": 1000,
-            "number_of_items": 4,
-            "time": "2024-01-19T19:00:00Z",
-        },
-    )
-    expected_delivery_fees = (200, 240, 240)
-
-    for expected_delivery_fee, query in zip(expected_delivery_fees, queries):
-        delivery_fee = delivery_fee_calculator(query)
-        delivery_fee_value = delivery_fee.get("delivery_fee")
-        assert delivery_fee_value == expected_delivery_fee
+    def check_delivery_fee(self, queries, expected_delivery_fees):
+        """Test all the queries and expected fees with the delivery fee calculator library."""
+        for query, expected_delivery_fee in zip(queries, expected_delivery_fees):
+            delivery_fee = delivery_fee_calculator(query)
+            self.assertEqual(delivery_fee.get("delivery_fee"), expected_delivery_fee)
