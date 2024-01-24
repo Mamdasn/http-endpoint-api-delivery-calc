adf0e4b6dd8828e62d9ab3b9ea352690cbab41f7
Mamdasan Sabrian <mhsaber1375@gmail.com>
2024-01-24 16:46:34 +0100

    Make EndpointTests inherit from FeeLogicTests
    + Override `check_delivery_fee` method to cross check the expected values with the response of HTTP endpoint

--- tests/test_http_endpoint.py
+++ tests/test_http_endpoint.py
@@ -1,10 +1,9 @@
-import unittest
-
 import requests
 from requests.exceptions import ConnectionError, Timeout
+from test_delivery_fee_logic import FeeLogicTests
 
 
-class EndpointTests(unittest.TestCase):
+class EndpointTests(FeeLogicTests):
     http_address = "http://0.0.0.0:5000"
 
     def test_connection(self):
@@ -33,155 +32,6 @@ class EndpointTests(unittest.TestCase):
 
         # Check if the request was aborted
         self.assertTrue(response.status_code == 400, "Invalid query parameters")
-
-    def test_fill_10_euro_gap_in_cart_value(self):
-        """Test the surcharge calculation for cart values less than 10€."""
-        queries = (
-            {
-                "cart_value": 890,
-                "delivery_distance": 1,
-                "number_of_items": 1,
-                "time": "2024-01-15T13:00:00Z",
-            },
-        )
-        expected_delivery_fees = (310,)
-        self.check_delivery_fee(queries, expected_delivery_fees)
-
-    def test_excess_500m_fee(self):
-        """Test additional delivery fees for every 500 meters beyond the first 1000 meters."""
-        queries = (
-            {
-                "cart_value": 1000,
-                "delivery_distance": 1000,
-                "number_of_items": 1,
-                "time": "2024-01-15T13:00:00Z",
-            },
-            {
-                "cart_value": 1000,
-                "delivery_distance": 1499,
-                "number_of_items": 1,
-                "time": "2024-01-15T13:00:00Z",
-            },
-            {
-                "cart_value": 1000,
-                "delivery_distance": 1500,
-                "number_of_items": 1,
-                "time": "2024-01-15T13:00:00Z",
-            },
-            {
-                "cart_value": 1000,
-                "delivery_distance": 1501,
-                "number_of_items": 1,
-                "time": "2024-01-15T13:00:00Z",
-            },
-        )
-        expected_delivery_fees = (200, 300, 300, 400)
-
-        self.check_delivery_fee(queries, expected_delivery_fees)
-
-    def test_excess_bulk_fee(self):
-        """Test the surcharge for bulk orders based on the number of items in the cart."""
-        queries = (
-            {
-                "cart_value": 1000,
-                "delivery_distance": 1000,
-                "number_of_items": 4,
-                "time": "2024-01-15T13:00:00Z",
-            },
-            {
-                "cart_value": 1000,
-                "delivery_distance": 1000,
-                "number_of_items": 5,
-                "time": "2024-01-15T13:00:00Z",
-            },
-            {
-                "cart_value": 1000,
-                "delivery_distance": 1000,
-                "number_of_items": 10,
-                "time": "2024-01-15T13:00:00Z",
-            },
-            {
-                "cart_value": 1000,
-                "delivery_distance": 1000,
-                "number_of_items": 13,
-                "time": "2024-01-15T13:00:00Z",
-            },
-            {
-                "cart_value": 1000,
-                "delivery_distance": 1000,
-                "number_of_items": 14,
-                "time": "2024-01-15T13:00:00Z",
-            },
-        )
-        expected_delivery_fees = (200, 250, 500, 770, 820)
-
-        self.check_delivery_fee(queries, expected_delivery_fees)
-
-    def test_15_euro_cap(self):
-        """Test the maximum delivery fee cap of 15€, including surcharges."""
-        queries = (
-            {
-                "cart_value": 1000,
-                "delivery_distance": 7001,
-                "number_of_items": 4,
-                "time": "2024-01-15T13:00:00Z",
-            },
-            {
-                "cart_value": 1000,
-                "delivery_distance": 7001,
-                "number_of_items": 12,
-                "time": "2024-01-15T13:00:00Z",
-            },
-        )
-        expected_delivery_fees = (1500, 1500)
-
-        self.check_delivery_fee(queries, expected_delivery_fees)
-
-    def test_200_euro_free_delivery(self):
-        """Test free delivery for cart values equal to or exceeding 200€."""
-        queries = (
-            {
-                "cart_value": 19999,
-                "delivery_distance": 1000,
-                "number_of_items": 4,
-                "time": "2024-01-15T13:00:00Z",
-            },
-            {
-                "cart_value": 20000,
-                "delivery_distance": 7001,
-                "number_of_items": 4,
-                "time": "2024-01-15T13:00:00Z",
-            },
-        )
-        expected_delivery_fees = (200, 0)
-
-        self.check_delivery_fee(queries, expected_delivery_fees)
-
-    def test_friday_rush(self):
-        """Test increased delivery fees during the Friday rush hours (3 - 7 PM UTC)."""
-        queries = (
-            {
-                "cart_value": 1000,
-                "delivery_distance": 1000,
-                "number_of_items": 4,
-                "time": "2024-01-19T14:59:59Z",
-            },
-            {
-                "cart_value": 1000,
-                "delivery_distance": 1000,
-                "number_of_items": 4,
-                "time": "2024-01-19T15:00:00Z",
-            },
-            {
-                "cart_value": 1000,
-                "delivery_distance": 1000,
-                "number_of_items": 4,
-                "time": "2024-01-19T19:00:00Z",
-            },
-        )
-        expected_delivery_fees = (200, 240, 240)
-
-        self.check_delivery_fee(queries, expected_delivery_fees)
 
     def check_delivery_fee(self, queries, expected_delivery_fees):
         """Test all the queries and expected fees with the http response."""
