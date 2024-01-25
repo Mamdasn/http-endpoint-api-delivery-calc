4ddc952129a00b80d9f6c720e5d184612d49c2fc
Mamdasan Sabrian <mhsaber1375@gmail.com>
2024-01-25 15:28:15 +0100

    Use absolute path to import `delivery_tools`
    + Add a docstring for the magic method `__doc__`
    + Assign the version numbering to the magic method `__version__`
    + Assign the names of methods contained within `delivery_tools` package to the magic method `__all__`

--- delivery_tools/__init__.py
+++ delivery_tools/__init__.py
@@ -1,4 +1,39 @@
 # delivery_tools/__init__.py
 # flake8: noqa
 
-from .delivery_tools import delivery_fee_calculator, delivery_query_integrity_check
+"""
+Package for calculating delivery fees and verifying delivery query integrity.
+
+This package includes utility functions to handle and calculate delivery fees
+based on various factors like cart value, delivery distance, number of items,
+and specific time considerations. It is designed to provide a robust and
+flexible way to apply different rules and surcharges associated with the
+delivery of goods.
+
+Modules:
+    delivery_tools: Provides functions for checking the integrity of delivery
+                    data and calculating delivery fees.
+
+Functions in delivery_tools:
+    delivery_query_integrity_check(data: dict) -> bool:
+        Checks if the delivery query contains all the required fields with
+        appropriate data types. Returns True if the data is valid, False otherwise.
+
+    delivery_fee_calculator(data: dict) -> dict:
+        Calculates the delivery fee based on the provided data, considering
+        factors like base fee, surcharges for distance and bulk orders, rush
+        hour charges, and caps on the maximum fee. Returns a dictionary with
+        the calculated fee.
+
+Example Usage:
+    from delivery_package import delivery_fee_calculator
+    fee_info = delivery_fee_calculator(order_data)
+"""
+
+from delivery_tools.delivery_tools import (
+    delivery_fee_calculator,
+    delivery_query_integrity_check,
+)
+
+__all__ = ["delivery_fee_calculator", "delivery_query_integrity_check"]
+__version__ = "1.0.0"
